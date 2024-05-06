36a504439232bde3366950c36c16a4c7b9781ac5
NavozZ <135834866+NavozZ@users.noreply.github.com>
2024-05-06 10:19:17 +0530

    question 3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Question4")]
+[assembly: AssemblyTitle("Question3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Question4")]
+[assembly: AssemblyProduct("Question3")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9f7b2931-17a4-4338-bb08-8148c79d9ef1")]
+[assembly: Guid("2fe32ba5-e90d-4aff-8b20-763f540009c2")]
 
 // Version information for an assembly consists of the following four values:
 //
